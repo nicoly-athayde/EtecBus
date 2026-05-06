072f68e330fb58bfb41116f7a753c82ae134550d
nicoly-athayde <nicolyathayde4@gmail.com>
2026-05-06 10:52:51 -0300

    terminar

--- EtecBus/App.js
+++ EtecBus/App.js
@@ -8,9 +8,38 @@ import * as Location from 'expo-location';
 const SCHOOL = {
   id: 'school',
   name: 'ETEC Comendador João Rays',
-  coordinate: {},
+  coordinate: { latitude: -22.489207558792767, longitude: -48.54638803159028},
   address: 'Rua Ludovico Victório, 2140, Barra Bonita - SP'
 };
+
+const BUS_STOPS = [
+  {
+    id: 'stop_1',
+    name: 'Autoescola Muriano',
+    address: 'R. Geraldo Fazzio,482',
+    coordinate:{ latitude: -22.484264161231977, longitude:-48.56482871771266},
+    lines:['Nova Barra'],
+  }
+];
+
+// Distância de Haversine ( metros)
+function getDistance(c1,c2) {
+
+
+}
+
+function formatDistance(m) {
+  return m <1000 ? ' ${Math.round(m)}'m' : ${(m/ 1000)}.toFixed(1)} Km';
+}
+//HTML do Leaflet (OpenStreepMap - sem chave)
+function buildLeafletHTML(userCoord, nearestStopId, selectedStopId) {
+const stopsJSON = JSON.stringify (BUS_STOPS);
+const schoolJSON = JSON.stringify (SCHOOL);
+const userJSON = userCoord ? JSON.stringify (userCoord) : 'null';
+
+ return '';
+
+}
 
 export default function App() {
   return (
